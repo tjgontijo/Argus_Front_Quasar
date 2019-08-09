ce4417d079b9a4a1220b06143c4e7e49e6c5a971
Thiago Gontijo <tjgontijo@gmail.com>
2019-08-09 13:00:05 -0300

    add create breed

--- src/pages/admin/breeds/create.vue
+++ src/pages/admin/breeds/create.vue
@@ -1,24 +1,27 @@
 <template>
   <q-page padding>
-      <div class="q-my-xl q-mx-lg">
-        <span class="text-center">
-          <div class="q-display-1">Cadastrar nova Raça, cor ou Etinia</div>
-        </span>
-        <div class="row q-mt-xl justify-center">
-          <div class="col-5 q-px-md">
-            <q-input
-              v-model="breed.name"
-              float-label="Raça, cor ou Etinia"
-              autofocus
-              @keyup.enter="submit"
-            />
-          </div>
-        </div>
-        <div class="row q-py-lg justify-between">
-                <q-btn color="warning" label="Voltar" block @click="$router.push('/admin/breeds')"/>
-                <q-btn color="primary" label="Salvar" block @click="submit"/>
+    <div class="q-my-xl q-mx-lg">
+      <q-card>
+        <q-card-title>Adicionar Raça, cor ou Etinia</q-card-title>
+        <q-card-separator />
+        <q-card-main>
+          <div class="row q-mt-xl justify-center">
+            <div class="col-5 q-px-md">
+              <q-input
+                v-model="breed.name"
+                float-label="Raça, cor ou Etinia"
+                autofocus
+                @keyup.enter="submit"
+              />
             </div>
-      </div>
+          </div>
+          <div class="row q-py-lg justify-between">
+            <q-btn color="warning" label="Voltar" block @click="$router.push('/admin/breeds')" />
+            <q-btn color="primary" label="Salvar" block @click="submit" />
+          </div>
+        </q-card-main>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
@@ -32,8 +35,8 @@ export default {
   methods: {
     submit () {
       const name = this.breed.name
-      this.$store.dispatch('breeds/create', { name }).then((response) => {
-        this.$router.push('/admin/breeds')
+      this.$store.dispatch("breeds/create", { name }).then(response => {
+        this.$router.push("/admin/breeds")
       })
     }
   }
