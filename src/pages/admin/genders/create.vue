e7ff5f702795a3e1a63577aa0aea4bb8aeaedc1b
Thiago Gontijo <tjgontijo@gmail.com>
2019-08-12 15:58:23 -0300

    Ajuste no layout

--- src/pages/admin/genders/create.vue
+++ src/pages/admin/genders/create.vue
@@ -1,24 +1,41 @@
 <template>
   <q-page padding>
-      <div class="q-my-xl q-mx-lg">
-        <span class="text-center">
-          <div class="q-display-1">Cadastrar novo Sexo</div>
-        </span>
-        <div class="row q-mt-xl justify-center">
-          <div class="col-5 q-px-md">
-            <q-input
-              v-model="gender.name"
-              float-label="Sexo"
-              autofocus
-              @keyup.enter="submit"
-            />
-          </div>
+    <div class="q-my-xl q-mx-lg">
+      <div class="row justify-center">
+        <div class="col-5">
+          <q-card>
+            <q-card-title>Adicionar</q-card-title>
+            <q-card-separator />
+            <q-card-main>
+              <div class="row q-mt-xl justify-center">
+                <div class="col-5 q-px-md">
+                  <q-input
+                    v-model="gender.name"
+                    float-label="Sexo"
+                    autofocus
+                    @keyup.enter="submit"
+                  />
+                </div>
+              </div>
+                <div class="row justify-between q-pt-xl">
+                  <q-btn
+                    color="warning"
+                    label="Voltar"
+                    block
+                    @click="$router.push('/admin/genders')"
+                  />
+                  <q-btn
+                    color="primary"
+                    label="Salvar"
+                    block
+                    @click="submit"
+                  />
+                </div>
+            </q-card-main>
+          </q-card>
         </div>
-        <div class="row q-py-lg justify-between">
-                <q-btn color="warning" label="Voltar" block @click="$router.push('/admin/genders')"/>
-                <q-btn color="primary" label="Salvar" block @click="submit"/>
-            </div>
       </div>
+    </div>
   </q-page>
 </template>
 
@@ -39,6 +56,3 @@ export default {
   }
 }
 </script>
-
-<style>
-</style>
